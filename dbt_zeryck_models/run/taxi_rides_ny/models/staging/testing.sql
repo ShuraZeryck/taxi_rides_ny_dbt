129d35df9809079033794e5b951f20ee791c6f04
Shura Zeryck <shura.zeryck@gmail.com>
2022-02-10 21:45:15 +0000

    Successfully running yellow data after installing dbt utils

--- dbt_zeryck_models/run/taxi_rides_ny/models/staging/testing.sql
+++
@@ -1,9 +0,0 @@
-
-
-  create or replace view `fresh-shell-338718`.`dbt_szeryck`.`testing`
-  OPTIONS()
-  as 
-
-select * from `fresh-shell-338718`.`trips_data_all`.`yellow_tripdata_partitioned_clustered`
-limit 100;
-
